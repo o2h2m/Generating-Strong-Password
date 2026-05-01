60c5ddbbe83c5055fec06147383237b29798f008
o2h2 <rrrooox2003@gmail.com>
2026-05-01 22:21:27 +0300

    Refactor password generation and length validation
    Storing the characters in char[] for memory safety.
    creating Fisher Yates algorithm to avoid converting StringBuilder to ArrayList just to use Collections.shuffle. 
     
    creating ensureEntropy method to check if the password has at least: one capital letter, one small letter, one digit, and one special symbol.
    
    Using java.awt.datatransfer to copy the password to the system clipboard, to make my life and your life a lot easier.
    
    
    Signed-off-by: o2h2 <rrrooox2003@gmail.com>

--- PasswordMehtods.java
+++ PasswordMehtods.java
@@ -1,31 +1,60 @@
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.regex.Pattern;
 
 public class PasswordMethods {
 
+    //I am going to create attributes to this project
     @SuppressWarnings("unused")
     private int length;
-    private final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    private final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+={}[],.";
     private final SecureRandom random = new SecureRandom();
     private final StringBuilder password = new StringBuilder();
 
-    @SuppressWarnings("unused")
     Boolean checkLength(int length){
-        return length >= 13 && length <= 41;
+        return length >= 18 && length <= 41;
     }
 
-    //method passwordGenerator
-    @SuppressWarnings("unused")
-    void passwordGenerator(int length){
-        List<String> ch = new ArrayList<>();
-        
+    //method GeneratePassword
+    void passwordGenerator(int length) throws InterruptedException{
+        password.setLength(0);
+        char[] randomChar = new char[length+1];
         for(int i = 0; i <= length; i++){
-            ch.add(characters.charAt(random.nextInt(characters.length()))+ "");
-            password.append(ch.get(i));
+            randomChar[i] = characters.charAt(random.nextInt(characters.length()));
+            password.append(randomChar[i]);
         }
-        Collections.shuffle(ch);
-        System.out.println("\nThe password is: "+password+"\n");
+        //Even more randomness!
+        fisherYates(password);
+
+        //Copying the password to the system clipboard 
+        StringSelection text = new StringSelection(password.toString());
+
+        if(!ensureEntropy(password)) passwordGenerator(length);
+        
+        else{
+            Clipboard systemClip = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+            systemClip.setContents(text, null);
+            //For some reason, I have to type Thread.sleep() to let the system register the data.
+            Thread.sleep(500);
+            System.out.println("\nThe password has been copied, check your system clipboard.\n");
+        }
+    }
+
+    //Ensures that the password has at lesat: one capital letter, one small letter, one digit, and one special symbol
+    private boolean ensureEntropy(StringBuilder password){
+        return Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$", password);
+    }
+
+    private void fisherYates(StringBuilder password){
+        for(int i = password.length()-1; i >0; i--){
+            int j = random.nextInt(i+1);
+
+            char temp = password.charAt(i);
+            password.setCharAt(i, password.charAt(j));
+            password.setCharAt(j, temp);
+        }
     }
 }
